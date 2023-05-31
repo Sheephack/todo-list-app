d12386b1c4859097473f06b0bf778a28c8dbafec
Matias Palladino <matias.palladino@gmail.com>
2023-05-31 15:12:11 -0300

    [UPDATE] Basic structure

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+export default function TodoList() {
+  return (
+    <div className="todo-container">
+        <ul className="todo-list"></ul>
+    </div>
+  )
+}
